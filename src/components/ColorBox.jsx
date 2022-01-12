1b37ce36171dcc3bdfa74d3cecda374b4d5754ef
Martin Luna <maluna@technisys.com>
2022-01-12 18:46:46 -0300

    Set timeout with useEffect, updated classnames

--- src/components/ColorBox.jsx
+++ src/components/ColorBox.jsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
+import cn from "classnames";
 import "./ColorBox.css";
 
 export default function ColorBox({ color, name }) {
+  const [copied, setCopied] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setCopied(false), 1500);
+  }, [copied]);
+
+  const handleCopyState = () => {
+    setCopied(true);
+  };
+
   return (
-    <CopyToClipboard text={color}>
+    <CopyToClipboard text={color} onCopy={handleCopyState}>
       <div className="ColorBox" style={{ backgroundColor: color }}>
+        <div
+          className={cn("ColorBox-overlay", {
+            active: copied,
+          })}
+          style={{ backgroundColor: color }}
+        />
+        <div
+          className={cn("ColorBox-message", {
+            active: copied,
+          })}
+        >
+          <h2 className="ColorBox-message-title">Color code copied!</h2>
+          <p className="ColorBox-message-text">{color}</p>
+        </div>
         <div className="ColorBox-container">
           <div className="ColorBox-name">
             <span>{name}</span>
