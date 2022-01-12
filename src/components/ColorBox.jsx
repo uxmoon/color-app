0af7fe4f31b61daa4676d2d6a5ea291a9b9799f6
Martin Luna <maluna@technisys.com>
2022-01-12 16:06:12 -0300

    Copy hex color using copy react library

--- src/components/ColorBox.jsx
+++ src/components/ColorBox.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import "./ColorBox.css";
 
 export default function ColorBox({ color, name }) {
   return (
-    <div className="ColorBox" style={{ backgroundColor: color }}>
-      <div className="ColorBox-container">
-        <div className="ColorBox-name">
-          <span>{name}</span>
+    <CopyToClipboard text={color}>
+      <div className="ColorBox" style={{ backgroundColor: color }}>
+        <div className="ColorBox-container">
+          <div className="ColorBox-name">
+            <span>{name}</span>
+          </div>
+          <button className="ColorBox-button">Copy</button>
         </div>
-        <button className="ColorBox-button">Copy</button>
+        <span className="ColorBox-more">More</span>
       </div>
-      <span className="ColorBox-more">More</span>
-    </div>
+    </CopyToClipboard>
   );
 }
