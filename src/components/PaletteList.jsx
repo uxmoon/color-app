637471fb28a3c945c204cfa00984d125f34db58e
Martin Luna <maluna@technisys.com>
2022-01-13 18:11:27 -0300

    Added minipalette component

--- src/components/PaletteList.jsx
+++ src/components/PaletteList.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import MiniPalette from "./MiniPalette";
 
 export default function PaletteList({ palettes }) {
   return (
     <div>
+      <MiniPalette />
       <p>Palette list</p>
       {palettes.map((item) => (
         <Link to={`/palette/${item.id}`} key={item.id}>
