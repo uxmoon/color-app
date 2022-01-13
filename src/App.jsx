73e287ad71ba2fa4863373bba40d8bccb2b6cbf0
Martin Luna <maluna@technisys.com>
2022-01-13 10:58:50 -0300

    New prop for Palette to pass generated color scales

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,11 @@ import { generatePalette } from "./helpers/colors";
 import seedColors from "./seedColors";
 
 function App() {
-  console.log(generatePalette(seedColors[2]));
   return (
     <div>
       <header>
         <h1>Color App</h1>
-        <Palette {...seedColors[2]} />
+        <Palette palette={generatePalette(seedColors[2])} />
       </header>
     </div>
   );
