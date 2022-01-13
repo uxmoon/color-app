2208ac46e6dbd79cfcf7016bff298fb4e946fe5c
Martin Luna <maluna@technisys.com>
2022-01-13 18:35:33 -0300

    Pass props to minipalette component

--- src/components/PaletteList.jsx
+++ src/components/PaletteList.jsx
@@ -5,12 +5,9 @@ import MiniPalette from "./MiniPalette";
 export default function PaletteList({ palettes }) {
   return (
     <div>
-      <MiniPalette />
       <p>Palette list</p>
-      {palettes.map((item) => (
-        <Link to={`/palette/${item.id}`} key={item.id}>
-          {item.paletteName}
-        </Link>
+      {palettes.map((palette) => (
+        <MiniPalette key={palette.id} {...palette} />
       ))}
     </div>
   );
