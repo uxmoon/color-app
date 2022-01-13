e551d637b3dac3a59a00752b94d8c19717103651
Martin Luna <maluna@technisys.com>
2022-01-13 16:26:30 -0300

    Added react-router-v4

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import Palette from "./components/Palette";
+import { Route, Switch } from "react-router-dom";
 import { generatePalette } from "./helpers/colors";
 import seedColors from "./seedColors";
 
 function App() {
-  return <Palette palette={generatePalette(seedColors[2])} />;
+  // return <Palette palette={generatePalette(seedColors[2])} />;
+  return (
+    <Switch>
+      <Route exact path="/" render={() => <h1>Home</h1>} />
+      <Route exact path="/palette/:id" render={() => <h1>Palette</h1>} />
+    </Switch>
+  );
 }
 
 export default App;
