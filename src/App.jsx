228100a0f87db1ac96fba1b6e9b2bf8335c62718
Martin Luna <maluna@technisys.com>
2022-01-13 11:44:20 -0300

    Clean up markup

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,7 @@ import { generatePalette } from "./helpers/colors";
 import seedColors from "./seedColors";
 
 function App() {
-  return (
-    <div>
-      <header>
-        <h1>Color App</h1>
-        <Palette palette={generatePalette(seedColors[2])} />
-      </header>
-    </div>
-  );
+  return <Palette palette={generatePalette(seedColors[2])} />;
 }
 
 export default App;
