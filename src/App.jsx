a528e44812e5d28e6b6e0c50eed16fad9bc067b4
Martin Luna <maluna@technisys.com>
2022-01-13 16:55:09 -0300

    Render palette component with findPalette id

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import Palette from "./components/Palette";
 import { Route, Switch } from "react-router-dom";
-import { generatePalette } from "./helpers/colors";
+import Palette from "./components/Palette";
 import seedColors from "./seedColors";
+import { generatePalette } from "./helpers/colors";
 
 function App() {
-  // return <Palette palette={generatePalette(seedColors[2])} />;
+  const findPalette = (id) => seedColors.find((palette) => palette.id === id);
+
   return (
     <Switch>
       <Route exact path="/" render={() => <h1>Home</h1>} />
-      <Route exact path="/palette/:id" render={() => <h1>Palette</h1>} />
+      <Route
+        exact
+        path="/palette/:id"
+        render={(routerProps) => (
+          <Palette
+            palette={generatePalette(findPalette(routerProps.match.params.id))}
+          />
+        )}
+      />
     </Switch>
   );
 }
