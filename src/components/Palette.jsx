95b34e4473758fe3edfd49fe9b6ccfc2d2ed0686
Martin Luna <maluna@technisys.com>
2022-01-13 10:57:59 -0300

    New prop for Palette with generated color scale

--- src/components/Palette.jsx
+++ src/components/Palette.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ColorBox from "./ColorBox";
 import "./Palette.css";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
 
 export default function Palette(props) {
-  const colorBoxes = props.colors.map((color) => (
-    <ColorBox key={color.color} color={color.color} name={color.name} />
+  const colorBoxes = props.palette.colors[500].map((color) => (
+    <ColorBox key={color.id} color={color.hex} name={color.name} />
   ));
   return (
     <div className="Palette">
       {/* Header - Navbar */}
+      <Slider />
       <div className="Palette-colors">{colorBoxes}</div>
       {/* Footer */}
     </div>
