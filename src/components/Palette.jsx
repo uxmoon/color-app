43fa900dc59a0a4f6b784b15219514b167704849
Martin Luna <maluna@technisys.com>
2022-01-13 11:25:03 -0300

    Update styles for slider

--- src/components/Palette.jsx
+++ src/components/Palette.jsx
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorBox from "./ColorBox";
-import "./Palette.css";
 import Slider from "rc-slider";
-import "rc-slider/assets/index.css";
 
-export default function Palette(props) {
-  const colorBoxes = props.palette.colors[500].map((color) => (
+import "rc-slider/assets/index.css";
+import "./Palette.css";
+
+export default function Palette({ palette }) {
+  const [level, setLevel] = useState(500);
+
+  const colorBoxes = palette.colors[level].map((color) => (
     <ColorBox key={color.id} color={color.hex} name={color.name} />
   ));
+
+  const handleLevel = (level) => {
+    // console.log(level);
+    setLevel(level);
+  };
+
   return (
     <div className="Palette">
       {/* Header - Navbar */}
-      <Slider />
+      <div className="slider">
+        <Slider
+          defaultValue={500}
+          min={100}
+          max={900}
+          step={100}
+          onAfterChange={handleLevel}
+        />
+      </div>
       <div className="Palette-colors">{colorBoxes}</div>
       {/* Footer */}
     </div>
