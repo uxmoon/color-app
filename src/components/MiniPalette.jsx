81ec8dde4122349b1777797a8da0fe15daf5e284
Martin Luna <maluna@technisys.com>
2022-01-17 15:14:11 -0300

    Start adding styles with MUI styled

--- src/components/MiniPalette.jsx
+++ src/components/MiniPalette.jsx
@@ -4,6 +4,8 @@ import { styled } from "@mui/material/styles";
 const Card = styled("div")`
   border: 1px solid #000;
   padding: 0.5rem;
+  background-color: #fff;
+  border-radius: 0.25rem;
 `;
 
 const CardFooter = styled("h2")`
--- src/components/PaletteList.jsx
+++ src/components/PaletteList.jsx
@@ -1,14 +1,39 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import MiniPalette from "./MiniPalette";
+import { styled } from "@mui/material/styles";
+
+const Page = styled("div")`
+  background-color: darkblue;
+  padding: 1rem;
+`;
+
+const Nav = styled("div")`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: #fff;
+`;
+
+const Main = styled("main")`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 1rem;
+`;
 
 export default function PaletteList({ palettes }) {
   return (
-    <div>
-      <p>Palette list</p>
-      {palettes.map((palette) => (
-        <MiniPalette key={palette.id} {...palette} />
-      ))}
-    </div>
+    <Page>
+      <header>
+        <Nav>
+          <h1>React colors</h1>
+          <p>text</p>
+        </Nav>
+      </header>
+      <Main>
+        {palettes.map((palette) => (
+          <MiniPalette key={palette.id} {...palette} />
+        ))}
+      </Main>
+    </Page>
   );
 }
